efe8d276f611daa918889404a00f6087b7a8fb76
Nils Charlois <nils.charlois@gmail.com>
2019-12-16 22:54:23 +0000

    Added game speed

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -6,9 +6,19 @@ public class Block : MonoBehaviour
 {
 	[SerializeField] AudioClip audioClip;
 	
+	// Cached reference
+	Level level;
+	
+	private void Start()
+	{
+		level = FindObjectOfType<Level>();
+		level.AddBreakableBlock();
+	}
+	
     private void OnCollisionEnter2D(Collision2D collision)
 	{
 		AudioSource.PlayClipAtPoint(audioClip, Camera.main.transform.position);
 		Destroy(gameObject);
+		level.DestroyOneBreakableBlock();
 	}
 }
---
+++ Block Breaker/Assets/Scripts/Level.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+	[SerializeField] int breakableBlocks; // serialized for debugging purposes
+	
+	// cached scene loader
+	SceneLoader sceneLoader;
+	
+	public void Start()
+	{
+		sceneLoader = FindObjectOfType<SceneLoader>();
+	}	
+	
+	public void AddBreakableBlock()
+	{
+		breakableBlocks ++ ;
+	}
+	
+	public void DestroyOneBreakableBlock()
+	{
+		breakableBlocks -- ;
+		if(breakableBlocks <= 0)
+		{
+			sceneLoader.LoadNextScene();
+		}
+	}
+}
